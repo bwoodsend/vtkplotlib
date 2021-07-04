355d7cf0f5f5b3eb83dd9889343782f34b2e0916
bwoodsend <bwoodsend@gmail.com>
2021-07-04 23:55:15 +0100

    Simplify vtkplotlib version handling.

--- setup.py
+++ setup.py
@@ -6,26 +6,18 @@ Created on Wed Jun 19 19:00:26 2019
 """
 
 from setuptools import setup, find_packages
-from distutils.command.build import build
 import sys
 from os import path
+import runpy
 
 HERE = path.split(__file__)[0]
 
 with open(path.join(HERE, "README.md"), "r") as fh:
     long_description = fh.read()
 
-with open(path.join(HERE, "version"), "r") as fh:
-    version = fh.read().strip().lstrip("v")
-
-
-class Build(build):
-
-    def run(self):
-        with open(path.join(HERE, "vtkplotlib", "__version__.py"), "w") as fh:
-            fh.write("# -*- coding: utf-8 -*-\n__version__ = \"{}\"\n"
-                     .format(version)) # yapf: disable
-        build.run(self)
+version = runpy.run_path(
+    path.join(HERE, "vtkplotlib", "__version__.py"),
+)["__version__"]
 
 setup(name='vtkplotlib',
       version=version,
@@ -52,7 +44,6 @@ setup(name='vtkplotlib',
                         "PyQt5" if sys.version_info.major >= 3 else "python_qt5",
                         "numpy-stl", "namegenerator", "PILLOW"],
       },
-      cmdclass={"build": Build},
       entry_points={
           "pyinstaller40": ["hook-dirs = vtkplotlib.data:_get_hooks_dir"]
       },
